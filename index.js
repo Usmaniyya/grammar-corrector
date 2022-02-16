dba2c14f6d4f901b032ce25133c5ca7785d520eb
Usman Yahaya <usmanyahaya6967@gmail.com>
2022-02-16 19:06:44 +0100

    my first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("btn").addEventListener('click', correct)
+
+function correct() {
+    var p = document.getElementById("paragraph");
+    p.style.textTransform = 'uppercase';
+}
